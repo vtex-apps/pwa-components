aabf389b8f0caaae598ec0a8c455e7324230332f
Thayanne <thayannevls@gmail.com>
2019-10-14 10:45:09 -0300

    add param type on setWebAppData

--- react/utils/webAppIndexedDB.ts
+++ react/utils/webAppIndexedDB.ts
@@ -1,6 +1,6 @@
 import { openDB } from 'idb' 
 
-const setWebAppData = async (key) => {
+const setWebAppData = async (key: string) => {
   const db = await openDB('webApp', 1)
   await db.put('webApp', {name: key, value: true}) 
 }
