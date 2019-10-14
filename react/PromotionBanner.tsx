8040867da35e453fa85580814c198a8bbb9c8289
Thayanne <thayannevls@gmail.com>
2019-10-14 10:41:48 -0300

    add constants and add UseCallback on handleAccept and handleDismiss

--- react/PromotionBanner.tsx
+++ react/PromotionBanner.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, InjectedIntlProps } from 'react-intl'
 
@@ -13,13 +13,17 @@ const propTypes = {
 }
 
 const CONSTANTS = {
+  TYPE_INSTALL: 'install',
+  TYPE_PUSH_NOTIFICATION: 'push-notification',
+  INSTALL_DISMISS_DB_NAME: 'appInstallDismissed',
+  PUSH_NOTIFICAION_DISMISS_DB_NAME: 'pushNotificationsDismissed',
   INSTALL_TITLE: 'store/promotion-banner.install.title',
   INSTALL_DESCRIPTION: 'store/promotion-banner.install.description',
   INSTALL_ACCEPT_BUTTON_LABEL: 'store/promotion-banner.install.button-accept.label',
   PUSH_NOTIFICATION_TITLE: 'store/promotion-banner.push-notification.title',
   PUSH_NOTIFICATION_DESCRIPTION: 'store/promotion-banner.push-notification.description',
   PUSH_NOTIFICATIONS_ACCEPT_BUTTON_LABEL: 'store/promotion-banner.push-notification.button-accept.label',
-  DISMISS_BUTTON_LABEL: 'store/promotion-banner.button-dismiss.label'
+  DISMISS_BUTTON_LABEL: 'store/promotion-banner.button-dismiss.label',
 }
 
 type Props = PropTypes.InferProps<typeof propTypes>
@@ -27,19 +31,21 @@ type Props = PropTypes.InferProps<typeof propTypes>
 const PromotionBanner: FC<Props & InjectedIntlProps> = ({ intl, type = 'install' }) => {
   const { showInstallPrompt = null } = usePWA() || {}
 
-  const handleDismiss = async () => {
-    if(type === 'install')
-      setWebAppData('appInstallDismissed')
+  const handleDismiss = useCallback(async () => {
+    if(type === CONSTANTS.TYPE_INSTALL)
+      setWebAppData(CONSTANTS.INSTALL_DISMISS_DB_NAME)
     else
-      setWebAppData('pushNotificationsDismissed')
-  }
+      setWebAppData(CONSTANTS.PUSH_NOTIFICAION_DISMISS_DB_NAME)
+  }, [type])
 
-  const handleAccept = () => {
-    if(type === 'install')
+  const handleAccept = useCallback(() => {
+    if(type === CONSTANTS.TYPE_INSTALL) {
       showInstallPrompt()
-    else
+    }
+    else {
       console.log('Push notifications are not implemented.')
-  }
+    }
+  }, [type, showInstallPrompt])
 
   return (
     <div className='pa6 br2 bg-muted-5 mb8 mw5 w-80-ns w-90 center fr-l'>
